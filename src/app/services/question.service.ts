a389ccb6fb157c37e8d9becde85c27e464f167ef
Florece Ann Robles <annr.dev@gmail.com>
2023-06-05 00:55:12 -0400

    Created question service

---
+++ src/app/services/question.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Question } from "../model/question.model";
+
+@Injectable()
+export class QuestionService {
+    endpoint = 'http://localhost:3000/api/questions';
+
+    constructor(
+        private http: HttpClient) {
+    }
+
+    add(question: Question) {
+        return this.http.post(this.endpoint, question).pipe();
+    }
+
+    get() {
+        return this.http.get<Question[]>(this.endpoint).pipe();
+    }
+
+    getById(index: number) {
+        return this.http.get<Question>(this.endpoint).pipe();
+    }
+
+    update(question: Question, index: number) {
+        return this.http.put(this.endpoint + "/" + index, question).pipe();
+    }
+
+    delete(index: number) {
+        return this.http.delete(this.endpoint + "/" + index).pipe();
+    }
+
+}
