991f242d04d01fa24c027c475982ab866f72bbfa
Florece Ann Robles <annr.dev@gmail.com>
2023-06-04 22:50:10 -0400

    Created home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+@UntilDestroy()
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {   
+  }
+
+  ngOnDestroy(){
+  }
+
+}
